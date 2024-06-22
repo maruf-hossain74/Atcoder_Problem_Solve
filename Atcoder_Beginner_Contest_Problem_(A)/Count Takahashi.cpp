76d8c2102958de294a1f347aa83b62944262c976
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-06-22 08:11:54 -0700

    AtCoder Beginner Contest 359

---
+++ Atcoder_Beginner_Contest_Problem_(A)/Count Takahashi.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  int ans = 0;
+  int t; cin>> t; while(t--) {
+    string s; cin>> s;
+    if(s == "Takahashi") ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
